60fd1a4dc3f5f4bd778531fca0757d5471bb8c44
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2025-01-25 09:34:32 +0200

    Adding password formatting in swagger configuration.

--- src/app.ts
+++ src/app.ts
@@ -38,8 +38,8 @@ export default class App {
 
     /*
     * todo::
-    *  1. Swagger configuration.
-    *   2. Prometheus and Grafana configuration
+    *
+    *    2. Prometheus and Grafana configuration
     *   3. Testing
     *   4. Asking the questions
     *   5. Revising about PM2 and why I need it
