9bbcf3e16ca1b936f1f5bac955f116a9d456d5ec
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2025-02-02 12:45:50 +0200

    fixed the stats controller.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import App from './app'
 import ChallengeRouter from "@routes/challenge-router";
 import AuthRouter from "@routes/auth-router";
+import AdminRouter from "@routes/admin-router";
 
 const app = new App(
-    [   new AuthRouter(),
+    [   new AdminRouter(),
+        new AuthRouter(),
         new ChallengeRouter()
     ]
 );
