4b40aa6c50054ff1b28bc8e68f6d5dfb25469b31
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2025-02-05 08:17:32 +0200

    Refining the instrumentation.

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,18 +1,26 @@
 import {NodeSDK} from "@opentelemetry/sdk-node";
-import {ConsoleSpanExporter} from "@opentelemetry/sdk-trace-node";
 import {getNodeAutoInstrumentations} from "@opentelemetry/auto-instrumentations-node";
-import {PrometheusExporter} from "@opentelemetry/exporter-prometheus";
-import {trace} from "@opentelemetry/api";
+import {Resource} from "@opentelemetry/resources";
+import {ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION} from "@opentelemetry/semantic-conventions";
+import {OTLPTraceExporter} from "@opentelemetry/exporter-trace-otlp-grpc";
+import {PeriodicExportingMetricReader} from "@opentelemetry/sdk-metrics";
+import {OTLPMetricExporter} from "@opentelemetry/exporter-metrics-otlp-grpc";
+import {SimpleLogRecordProcessor} from "@opentelemetry/sdk-logs";
+import {OTLPLogExporter} from "@opentelemetry/exporter-logs-otlp-grpc";
 
 const sdk = new NodeSDK({
-    traceExporter: new ConsoleSpanExporter(),
-    metricReader: new PrometheusExporter({
-        port: 9464
+    resource: new Resource({
+        [ATTR_SERVICE_NAME]: "umurava-backend",
+        [ATTR_SERVICE_VERSION]: "1.0",
     }),
-    instrumentations: [getNodeAutoInstrumentations()]
-})
+    traceExporter: new OTLPTraceExporter({
+        url: "http://otel-collector:4317",
+    }),
+    metricReader: new PeriodicExportingMetricReader({
+        exporter: new OTLPMetricExporter()
+    }),
+    logRecordProcessors: [new SimpleLogRecordProcessor(new OTLPLogExporter())],
+    instrumentations: [getNodeAutoInstrumentations()],
+});
 
-sdk.start()
-
-const tracer = trace.getTracer('umurava-app')
-export {tracer}
+sdk.start();
